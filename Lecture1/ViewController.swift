7ed0932735fbf89d1d460836fc18449d58dd5eaf
admin <admin@test.local>
2016-07-03 12:30:57 +0300

    Add new button

--- Lecture1/ViewController.swift
+++ Lecture1/ViewController.swift
@@ -26,6 +26,9 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func newButtonTaped(sender: AnyObject) {
+    }
+    
     @IBAction func buttonTaped(sender: AnyObject) {
         if let text = myText.text {
             myLabel.text = "Hello, " + text
